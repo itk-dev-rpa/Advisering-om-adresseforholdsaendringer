c0778e3c5574d6237d1ce2fb8b0ea1409beb1066
Mathias G <ghbm@aarhus.dk>
2026-04-28 11:13:30 +0200

    Remove `if __name__ == "__main__"` blocks

--- robot_framework/process.py
+++ robot_framework/process.py
@@ -235,10 +235,3 @@ def clear_email_folder(orchestrator_connection: OrchestratorConnection):
     emails = mail.get_emails_from_folder("itk-rpa@mkb.aarhus.dk", "Indbakke/AKBO", graph_access)
     for email in emails:
         mail.delete_email(email, graph_access)
-
-
-if __name__ == '__main__':
-    conn_string = os.getenv("OpenOrchestratorConnString")
-    crypto_key = os.getenv("OpenOrchestratorKey")
-    oc = OrchestratorConnection("Akbo Test", conn_string, crypto_key, "ghbm@aarhus.dk,itk-rpa@mkb.aarhus.dk", "", "")
-    process(oc)
